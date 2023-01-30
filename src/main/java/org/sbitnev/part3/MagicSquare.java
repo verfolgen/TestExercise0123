ef678ce7933aa464cda74436b5d68c49b7fe9afc
evgeny sbitnev <zhe@yandex.ru>
2023-01-30 11:13:52 +0400

    begin task

--- src/main/java/org/sbitnev/part3/MagicSquare.java
+++ src/main/java/org/sbitnev/part3/MagicSquare.java
@@ -3,31 +3,38 @@ package org.sbitnev.part3;
 import java.util.Scanner;
 
 public class MagicSquare {
+    public static void main(String[] args) {
+        int [] [] result = {{4, 9, 2}, {3, 5, 7}, {8, 1, 5}};
+        System.out.println(MagicSquare.magicSquarerTranform(result));
+    }
 
-    public static int [] [] magicSquarerTranform(int [] [] initialArray) {
-        int [] [] magicArray = new int [3] [3];
+    public static int magicSquarerTranform(int [] [] initialArray) {
 
-        Scanner sc = new Scanner(System.in);
+        int [] [] squares = new int [] [] {
+                {8, 3, 4, 1, 5, 9, 6, 7, 2},
+                {4, 3, 8, 9, 5, 1, 2, 7, 6},
+                {8, 1, 6, 3, 5, 7, 4, 9, 2},
+                {6, 1, 8, 7, 5, 3, 2, 9, 4},
+                {2, 9, 4, 7, 5, 3, 6, 1, 8},
+                {4, 9, 2, 3, 5, 7, 8, 1, 6},
+                {2, 7, 6, 9, 5, 1, 4, 3, 8},
+                {6, 7, 2, 1, 5, 9, 8, 3, 4}
+        };
 
-        int sumDia = 0, sumRow = 0, sumCol = 0;
+        int min = Integer.MAX_VALUE;
 
-        for(int i = 0; i < 3; i ++){
-            for(int j = 0; j <3; j ++) {
-                System.out.println("Enter number " + "[" + i + "]" + " [" + j + "]");
-                magicArray [i] [j] = sc.nextInt();
-
+        for(int i = 0; i < squares.length; i++) {
+            int total = 0;
+            for(int j = 0; j < squares[i].length; j++) {
+                total += Math.abs(initialArray[j/3] [j % 3] - squares [i] [j]);
+            }
+            if(total < min) {
+                min = total;
             }
         }
 
-        for(int i = 0; i < 3; i ++) {
-            for (int j = 0; j < 3; j ++) {
-                if (i == j) {
-                    sumDia
-                }
-            }
-        }
 
         int [] [] result = {{4, 3, 78}, {2, 7, 6}, {9, 5, 1}};
-        return  result;
+        return  min;
     }
 }
